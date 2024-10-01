232a09550b96f223fd72b149a169acd872acb1f5
Jacob Lawrence <71344853+JacobWLaw@users.noreply.github.com>
2024-10-01 14:20:50 -0400

    Update compare_logic.py

--- compare_logic.py
+++ compare_logic.py
@@ -2,10 +2,9 @@ from PIL import Image, ImageChops, ImageOps
 
 class image_compare():
     
-    def comparison(self, baseline_data, actual_image):
+    def comparison(self, baseline_data, actual_image, threshold):
         baseline = Image.open(baseline_data)
         actual = Image.open(actual_image)
-        Threshold = 99.00 #change this to whatever your threshold is, I.E if you are okay with images that are only x amount similar
         
         diff = ImageChops.difference(baseline, actual)
         num_different_pixels = sum(1 for pixel in diff.getdata() if pixel != (0, 0, 0))
@@ -18,9 +17,9 @@ class image_compare():
 
         print(f"Percent difference: {difference_percentage:.2f}%")
 
-        if difference_percentage < Threshold:  
+        if difference_percentage < threshold:  
             diff_gray = diff.convert("L")
-            diff_highlighted = ImageOps.colorize(diff_gray, black="black", white="red")
+            diff_highlighted = ImageOps.colorize(diff_gray, black="black", white="white")
             return diff_highlighted, difference_percentage
         else:
             return None
